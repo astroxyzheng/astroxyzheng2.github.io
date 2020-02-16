ac9cd75a9d618cd00c4f4f9ead917f4bf47bbb95
astroxyzheng <52479837+astroxyzheng@users.noreply.github.com>
2020-02-17 05:19:17 +0800

    Create scripts.js

---
+++ _js/scripts.js
@@ -0,0 +1,54 @@
+/*eslint-env jquery*/
+
+// Jquery & Velocity JS included in GULP
+$( document ).ready( function() {
+
+    toggleMobileNav();
+    ShowHideNav();
+    formCheck();
+
+} );
+
+
+
+$( window ).resize( function() {
+    $( ".header" ).removeClass( "hide-nav" ); // Ensure nav will be shown on resize
+    $( ".header__links" ).removeAttr( "style" ); // If mobile nav was collapsed, make sure it's show on DESK
+    $( ".header__overlay" ).remove();
+} );
+
+// Toggle Mobile Navigation
+function toggleMobileNav() {
+    $( ".header__toggle" ).click( function() {
+
+        if ( $( ".header__links" ).hasClass( "js--open" ) ) {
+            hideMobileNav();
+        }
+        else {
+            openMobileNav();
+        }
+    } );
+
+    $( ".header__overlay" ).click( function() {
+        hideMobileNav();
+    } );
+}
+
+function openMobileNav() {
+    $( ".header__links" ).velocity( "slideDown", {
+        duration: 300,
+        easing: "ease-out",
+        display: "block",
+        visibility: "visible",
+        begin: function() {
+            $( ".header__toggle" ).addClass( "--open" );
+            $( "body" ).append( "<div class='header__overlay'></div>" );
+        },
+        progress: function () {
+            $( ".header__overlay" ).addClass( "--open" );
+        },
+        complete: function() {
+            $( this ).addClass( "js--open" );
+        }
+    } );
+}
